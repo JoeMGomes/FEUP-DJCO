887c1ad869e29702154d1b795666062e4e11849d
JoeMGomes <zemiguelgomes99@gmail.com>
2021-02-11 22:49:42 +0000

    Add arm rotation and crude player sprite

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,15 +6,19 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public Rigidbody2D m_rigidbody;
+    
     public float moveSpeed = 3.0f;
     public float jumpHeight = 7.0f;
     public float groundDist;
     public LayerMask floorLayer;
 
+    public GameObject armParent;
+    public GameObject bodyParent;
+
     private void Awake()
     {
         m_rigidbody = GetComponent<Rigidbody2D>();
-
+        
     }
 
     private void Start()
@@ -38,6 +42,29 @@ public class PlayerMovement : MonoBehaviour
         {
             m_rigidbody.AddForce(new Vector2(0, jumpHeight), ForceMode2D.Impulse);
         }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - armParent.transform.position;
+
+        diff.Normalize();
+
+        float rotation = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+
+        armParent.transform.rotation = Quaternion.Euler(0f, 0f, rotation);
+        bodyParent.transform.localRotation = Quaternion.Euler(0, 0, 0);
+        //Mouse to the left side of the player
+        if (rotation < -90 || rotation > 90)
+        {
+            if (armParent.transform.eulerAngles.y == 0)
+            {
+                armParent.transform.localRotation = Quaternion.Euler(180, 0, -rotation);
+                bodyParent.transform.localRotation = Quaternion.Euler(0, 180, 0);
+                
+            }
+           
+        } 
     }
 
     bool IsGrounded()
