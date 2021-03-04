c5033ebe11b63a6f3dbad9c1b3d603cebf96edbf
Henrique Freitas <up201707046@fe.up.pt>
2021-03-04 16:02:31 +0000

    Camera follow player and completed level

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    public Transform cameraPos;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraPos.position = new Vector3(player.transform.position.x, cameraPos.position.y, cameraPos.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cameraPos.position = new Vector3(player.transform.position.x, cameraPos.position.y, cameraPos.position.z);
+    }
+}
