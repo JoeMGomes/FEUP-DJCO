9ada7121612b2e95512c5597a84ba6534baee4d2
JoeMGomes <zemiguelgomes99@gmail.com>
2021-03-19 13:14:44 +0000

    Add scrollable backgorund for main menu

---
+++ Assets/Scripts/Scroll_Background.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Scroll_Background : MonoBehaviour
+{
+
+    public float speed = 1.0f;
+    private MeshRenderer rend;
+    private void Start()
+    {
+        rend = GetComponent<MeshRenderer>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        rend.material.SetTextureOffset("_MainTex", new Vector2(Time.time * speed, 0f));
+    }
+}
