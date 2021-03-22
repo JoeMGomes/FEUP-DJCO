e501bfe1aec8608c92217ff8432b4476fe05227f
JoeMGomes <zemiguelgomes99@gmail.com>
2021-03-22 18:58:07 +0000

    Add cleaning lady collision and flinch

---
+++ Assets/Scripts/CleaningLady.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CleaningLady : MonoBehaviour
+{
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Enemy"))
+        {
+            Physics2D.IgnoreCollision(GetComponent<Collider2D>(), collision.collider);
+
+        }
+        if (collision.collider.CompareTag("Player"))
+        {
+            collision.collider.GetComponent<PlayerMovement>().Flinch(gameObject.transform.position);
+
+        }
+    }
+
+}
